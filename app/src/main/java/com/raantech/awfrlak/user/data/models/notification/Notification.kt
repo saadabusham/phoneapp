96f1c8065cde624c315dda34712de161a5d9c712
sa2ed <sa2edabusham87@gmail.com>
2022-06-04 16:28:52 +0300

    under dev

--- app/src/main/java/com/raantech/awfrlak/user/data/models/notification/Notification.kt
+++ app/src/main/java/com/raantech/awfrlak/user/data/models/notification/Notification.kt
@@ -1,7 +1,28 @@
 package com.raantech.awfrlak.user.data.models.notification
 
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
 data class Notification(
-        val id: Int = 0,
-        val title: String,
-        val read :  Boolean = false
-)
+        @field:SerializedName("created_at_diff_for_humans")
+        val createdAtDiffForHumans: String? = null,
+
+        @field:SerializedName("type_id")
+        val typeId: Any? = null,
+
+        @field:SerializedName("created_at")
+        val createdAt: String? = null,
+
+        @field:SerializedName("id")
+        val id: String? = null,
+
+        @field:SerializedName("type")
+        val type: String? = null,
+
+        @field:SerializedName("title")
+        val title: String? = null,
+
+        @field:SerializedName("body")
+        val body: String? = null,
+        val read: Boolean = false
+):Serializable
